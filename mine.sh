10ed5e18cacb3b2ef93b58a83ce504b51e8afd66
Ipeng13 <95187786+Ipeng13@users.noreply.github.com>
2025-06-15 19:44:04 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RKutTNM1L58h9B8tTt6LHGSYhxQqwtUUtP.agtRig -p x --cpu "$(nproc)"
